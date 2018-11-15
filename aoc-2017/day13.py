749dcd83d12490983fda3120770e040f465e8d89
Milco Numan <milco.numan@gmail.com>
2018-11-15 15:10:50 +0100

    Refactoring failed, started day 13 ...

---
+++ aoc-2017/day13.py
@@ -0,0 +1,76 @@
+"""Functions for Advent of Code 2017, day 13"""
+
+
+class Layer():
+    """Layer objects for the firewall"""
+
+    def __init__(self, depth):
+        """Initialize a new layer object, containing pos(ition), depth and move offset
+           attributes.
+        """
+        self.depth = depth
+        if depth is not None and depth > 0:
+            self.pos = 0
+            self.offset = 1
+        else:
+            self.pos = None
+            self.offset = None
+
+    def move(self):
+        """ Current position determines the move direction, this is evaluated just
+           before the move is executed.
+        """
+        if self.pos is not None:
+            if self.pos == 0:
+                self.offset = 1
+            elif self.pos == self.depth - 1:
+                self.offset = -1
+            self.pos += self.offset
+
+
+class Firewall():
+    def __init__(self, layers):
+        self.layers = layers
+        self.packet_position = -1  # still outside firewall
+        self.packet_score = 0
+
+    def move_scanners(self):
+        """Move all scanners one step"""
+        for layer in self.layers:
+            layer.move()
+
+    def move_packet(self):
+        """Simply move the packet ahead in the firewall ... or outside of if :-) """
+        self.packet_position += 1
+
+    def cycle(self):
+        """Move packet and if still in firewall,
+           determine it has moved onto a scanner.
+           If so, increment the score, then move all 
+           scanners.
+        """
+        self.move_packet()
+        if self.packet_position < len(self.layers):
+            if self.layers[self.packet_position].pos == 0:
+                # caught!
+                self.packet_score += self.packet_position * \
+                    self.layers[self.packet_position].depth
+        self.move_scanners()
+
+
+def read_configuration(filename):
+    """Parse configuration file and return a Firewall
+       object containing the problem domain, a firewall
+       having multiple layers with scanners.
+    """
+    with open(filename) as f:  # pylint: disable=C0103
+        content = f.readlines()
+    firewall = []
+    for line in content:
+        elements = line.split(':')
+        pos = int(elements[0].strip())
+        depth = int(elements[1].strip())
+        while pos > len(firewall):
+            firewall.append(Layer(None))
+        firewall.append(Layer(depth))
+    return Firewall(firewall)
---
+++ aoc-2017/day13_test.py
@@ -0,0 +1,73 @@
+"""Test module for functions of Advent of Code, day13"""
+import os
+import day13
+# pylint: disable=C0111,C0303
+
+
+def test_read_file_test():
+    myfirewall = day13.read_configuration(os.path.join(
+        os.path.dirname(__file__), 'day13_test.txt'))
+    assert len(myfirewall.layers) == 7
+    assert myfirewall.layers[0].depth == 3
+    assert myfirewall.layers[1].depth == 2
+    assert myfirewall.layers[2].depth is None
+    assert myfirewall.layers[3].depth is None
+    assert myfirewall.layers[4].depth == 4
+    assert myfirewall.layers[5].depth is None
+    assert myfirewall.layers[6].depth == 4
+    # not yet started
+    assert myfirewall.packet_position == -1
+    assert myfirewall.packet_score == 0
+
+
+def test_move():
+    myfirewall = day13.read_configuration(os.path.join(
+        os.path.dirname(__file__), 'day13_test.txt'))
+    # all position on origin
+    assert myfirewall.layers[0].pos == 0
+    assert myfirewall.layers[1].pos == 0
+    assert myfirewall.layers[2].pos is None
+    assert myfirewall.layers[3].pos is None
+    assert myfirewall.layers[4].pos == 0
+    assert myfirewall.layers[5].pos is None
+    assert myfirewall.layers[6].pos == 0
+    myfirewall.move_scanners()
+    # all positions have moved
+    assert myfirewall.layers[0].pos == 1
+    # this is the maximum depth, hence invert after this step !
+    assert myfirewall.layers[1].pos == 1
+    assert myfirewall.layers[2].pos is None
+    assert myfirewall.layers[3].pos is None
+    assert myfirewall.layers[4].pos == 1
+    assert myfirewall.layers[5].pos is None
+    assert myfirewall.layers[6].pos == 1
+    myfirewall.move_scanners()
+    # slot 0 must moved to maxdepth
+    assert myfirewall.layers[0].pos == 2
+    # slot 1 must have returned to origin
+    assert myfirewall.layers[1].pos == 0
+    myfirewall.move_scanners()
+    # slot 0 has inverted and stepped back to pos 2 (=1)
+    assert myfirewall.layers[0].pos == 1
+    # slot 1 has moved forward again
+    assert myfirewall.layers[1].pos == 1
+    # three steps taken
+    assert myfirewall.layers[4].pos == 3
+
+
+def test_pass_through():
+    myfirewall = day13.read_configuration(os.path.join(
+        os.path.dirname(__file__), 'day13_test.txt'))
+    while myfirewall.packet_position < len(myfirewall.layers):
+        myfirewall.cycle()
+    # caught at layer 0 - depth 3 and layer 6 - depth 4
+    assert myfirewall.packet_score == 24
+
+
+def test_pass_through_2():
+    myfirewall = day13.read_configuration(os.path.join(
+        os.path.dirname(__file__), 'day13_test2.txt'))
+    while myfirewall.packet_position < len(myfirewall.layers):
+        myfirewall.cycle()
+    # caught at layer 0, 2, 4, 6, 8 all depth 2
+    assert myfirewall.packet_score == (0 + 2 + 4 + 6 + 8) * 2
